71f5e7573a9ad2c720a2b61eec1e29a8a920af1e
Filip Pobořil <tsuki@fpob.eu>
2017-04-29 21:39:25 +0200

    i3, conky

---
+++ .config/conky/mpris-info.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# http://stackoverflow.com/questions/4545661/unicodedecodeerror-when-redirecting-to-file
+export PYTHONIOENCODING=UTF-8
+
+status=`mpris-remote playstatus | awk '/^playing/{print $2}'`
+
+if [[ $status == playing ]] ; then
+    awk_script='{
+        $1=""
+        gsub(/^\s+/, "", $0)
+        if (length($0) > 32)
+            print substr($0,0,32) "\\\\u2026"
+        else
+            print
+    }'
+    artist=`mpris-remote trackinfo | awk "/^artist/$awk_script"`
+    title=`mpris-remote trackinfo | awk "/^title/$awk_script"`
+    [[ -n $artist$title ]] && echo "\\uf001 $artist \\u2013 $title"
+fi 2>/dev/null
---
+++ .config/i3/README.md
@@ -0,0 +1,11 @@
+Change mate window manager
+
+```
+gsettings set org.mate.session.required-components windowmanager i3
+```
+
+Prevent Caja from managing the desktop
+
+```
+gsettings set org.mate.background show-desktop-icons false
+```
