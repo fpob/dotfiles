ecea3be15809b74968b72c5d0861a5064a04e6b0
Filip Pobořil <tsuki@fpob.eu>
2018-03-05 13:58:08 +0100

    tmux, vim

--- .tmux/environments-clean.sh
+++ .tmux/environments-clean.sh
@@ -6,5 +6,5 @@ tmux ls &>/dev/null || exit
 # Older than 7 days
 #find "$HOME/.tmux/environments" -type f -mtime +6 -delete
 
-# Keep only last 48 environments
-find "$HOME/.tmux/environments" -type f | sort -rn | tail -n +49 | xargs rm -rf
+# Keep only last 10 environments
+find "$HOME/.tmux/environments" -type f | sort -rn | tail -n +11 | xargs rm -rf
--- .tmux/environments-fix.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd $HOME/.tmux/environments
-last=$(basename "$(find "$HOME/.tmux/environments" -type f | sort -n | tail -n 1)")
-ln -sf "$last" last
