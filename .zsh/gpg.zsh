96cdbffb47560eadee47a19604a7acc3cdef83b7
Filip Poboril <fpoboril@redhat.com>
2019-06-25 21:12:43 +0200

    zsh: gpg utils

--- .zsh/gpg.zsh
+++ .zsh/gpg.zsh
@@ -6,7 +6,21 @@ gpg-source () {
     source =(gpg-cat $@)
 }
 
-# Execute GPG encrypted shell file.
-gpg-sh () {
-    gpg-cat $@ | $SHELL
+# Execute GPG encrypted file. By default executes in current running shell.
+# Shell can be changed by '-s SHELL' option.
+gpg-run () {
+    local shell=$SHELL
+
+    while getopts s: opt ; do
+        case $opt in
+            s) shell=$OPTARG ;;
+            *) echo "Unknown option '$opt'" ;;
+        esac
+    done
+    shift $((--OPTIND))
+
+    local script=$1
+    shift 1
+
+    $shell =(gpg-cat "$script") $@
 }
