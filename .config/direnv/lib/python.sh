4d84c6124a8f2050539e3260d9861b5b52db6938
Filip Poboril <fpoboril@redhat.com>
2022-04-25 17:23:20 +0200

    direnv: fix python

--- .config/direnv/lib/python.sh
+++ .config/direnv/lib/python.sh
@@ -1,18 +1,27 @@
 layout_python() {
-    local python=${1:-python3}
+    local python="${2:-python3}"
     shift
 
-    unset PYTHONHOME
-    VIRTUAL_ENV=$(direnv_layout_dir)/$python
+    # Resolve '$python' to its real path/name, in case it is symlink.
+    local python_path="$(realpath "$(which "$python")")"
+    if [[ -z "$python_path" ]]; then
+        log_error "Python '$python' not found in PATH"
+        return 1
+    fi
 
-    local ve=$($python -c "import pkgutil as p; print('venv' if p.find_loader('venv') else ('virtualenv' if p.find_loader('virtualenv') else ''))")
-    if [[ -z $ve ]] ; then
+    python="$(basename "$python_path")"
+
+    unset PYTHONHOME
+    VIRTUAL_ENV="$(direnv_layout_dir)/$python"
+
+    local ve="$("$python" -c "import pkgutil as p; print('venv' if p.find_loader('venv') else ('virtualenv' if p.find_loader('virtualenv') else ''))")"
+    if [[ -z "$ve" ]] ; then
         log_error "Neither 'venv' nor 'virtualenv' are available."
         return 1
     fi
 
-    if [[ ! -d $VIRTUAL_ENV ]]; then
-        $python -m $ve "$@" "$VIRTUAL_ENV"
+    if [[ ! -d "$VIRTUAL_ENV" ]]; then
+        "$python" -m "$ve" "$@" "$VIRTUAL_ENV"
     fi
 
     export VIRTUAL_ENV
