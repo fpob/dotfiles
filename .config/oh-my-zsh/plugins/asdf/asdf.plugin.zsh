355b1a89aaf35a8cafdb722c658d4bfd4afdd43a
Filip Pobořil <tsuki@fpob.cz>
2022-06-20 09:42:42 +0200

    zsh: cleanup + starship

--- .config/oh-my-zsh/plugins/asdf/asdf.plugin.zsh
+++
@@ -1,6 +0,0 @@
-export ASDF_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/asdf"
-export ASDF_DATA_DIR="$ASDF_DIR"
-export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME:-$HOME/.config}/asdf/asdfrc"
-
-path=("$ASDF_DIR/bin" $path)
-export PATH
--- .config/oh-my-zsh/plugins/kubectl/kubectl.plugin.zsh
+++
@@ -1,7 +0,0 @@
-if (( $+commands[kubectl] )); then
-    completion_file="$ZSH_CACHE_DIR/completions/_kubectl"
-    if [[ ! -f "$completion_file" ]] ; then
-        kubectl completion zsh > "$completion_file"
-    fi
-    unset completion_file
-fi
