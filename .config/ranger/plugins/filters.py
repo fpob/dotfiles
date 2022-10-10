5dcb0758d032e476efc4bd5c9e01070d90475af6
Filip Pobořil <tsuki@fpob.cz>
2022-10-10 19:40:24 +0200

    ranger: remove xattr filter

--- .config/ranger/plugins/filters.py
+++ .config/ranger/plugins/filters.py
@@ -1,7 +1,5 @@
 import re
 
-import xattr
-
 import ranger.api
 from ranger.core.filter_stack import stack_filter, BaseFilter, FileManagerAware
 
@@ -36,27 +34,6 @@ class SelectedFilter(BaseFilter, FileManagerAware):
         return '<Filter: selected>'
 
 
-@stack_filter('xattr')
-class DmetaFilter(BaseFilter):
-    def __init__(self, name_pattern):
-        self.name, self.pattern = name_pattern.split(maxsplit=1)
-        self.regex = re.compile(self.pattern)
-
-    def __call__(self, fobj):
-        try:
-            value = xattr.getxattr(fobj.path, self.name).decode()
-            return self.regex.search(value)
-        except OSError as e:
-            if e.errno == 61:   # No data available (xattr key not set).
-                return False
-            raise
-        except UnicodeError:
-            return False
-
-    def __str__(self):
-        return f'<Filter: xattr {self.name} =~ /{self.pattern}/>'
-
-
 old_hook_init = ranger.api.hook_init
 
 
@@ -64,7 +41,6 @@ def init(fm):
     fm.execute_console('map .t<any> filter_stack add tag %any')
     fm.execute_console('map .T filter_stack add tag')
     fm.execute_console('map .s filter_stack add selected')
-    fm.execute_console('map .x console filter_stack add xattr%space')
     return old_hook_init(fm)
 
 
